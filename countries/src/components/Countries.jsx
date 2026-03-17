985fed64f8c7881fb0d2c73727612f7d4c06dc0d
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-17 15:21:13 +0100

    done

--- countries/src/components/Countries.jsx
+++ countries/src/components/Countries.jsx
@@ -11,12 +11,15 @@ const Countries = () => {
         const fetchCountries = async () => {
             try {
                 const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,population,currencies,region");
-                setCountries(response.data);
-                setLoading(false)
+                setTimeout(() => {
+                    setCountries(response.data);
+                    setLoading(false)
+                }, 2000);
             } catch (error) {
                 console.log(error);
-
-                setLoading(false)
+                setTimeout(() => {
+                    setLoading(false)
+                }, 2000);
             }
         };
         fetchCountries();
@@ -26,39 +29,41 @@ const Countries = () => {
         const matchesSearch = country.name.common
             .toLowerCase()
             .includes(search.toLowerCase());
-    const matchesRegion = region ? country.region === region : true;
-    return matchesSearch && matchesRegion;
+        const matchesRegion = region ? country.region === region : true;
+        return matchesSearch && matchesRegion;
 
     });
 
-    if (loading) return <p>Loading...</p>
+    if (loading) return <p className='text-center my-5'>Loading...</p>
     return (
         <div>
-            <div className='flex justify-center items-center'>
-                <h1>Countries</h1>
-                <input
-                    type="text"
-                    placeholder="Search for a country..."
-                    className="border p-2 mb-5 w-full md:w-1/3"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
+            <div className='flex justify-center items-center flex-col '>
+                <h1 className='text-6xl my-10'>Countries</h1>
+                <div className='flex flex-wrap justify-center items-center w-full gap-5 mx-auto px-10'>
+                    <input
+                        type="text"
+                        placeholder="Search for a country..."
+                        className="border p-2 mb-5 w-full md:w-[50%] outline-0 rounded-sm"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
 
-                <select
-                    className="border p-2 mb-5 w-full md:w-1/4"
-                    value={region}
-                    onChange={(e) => setRegion(e.target.value)}
-                >
-                    <option value="">All Regions</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Americas">Americas</option>
-                    <option value="Oceania">Oceania</option>
-                    <option value="Antarctica">Antarctica</option>
-                </select>
+                    <select
+                        className="border p-2 mb-5 w-30% rounded-sm outline-0"
+                        value={region}
+                        onChange={(e) => setRegion(e.target.value)}
+                    >
+                        <option value="">Select Regions</option>
+                        <option value="Africa">Africa</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Americas">Americas</option>
+                        <option value="Oceania">Oceania</option>
+                        <option value="Antarctica">Antarctica</option>
+                    </select>
+                </div>
             </div>
-            <div className='grid grid-cols-4 justify-items-center gap-5 px-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-5 px-10 shrink-0'>
                 {filteredCountries.map((country, index) => {
                     const currency = country.currencies
                         ? Object.values(country.currencies)[0]
@@ -67,17 +72,17 @@ const Countries = () => {
                     const regionName = country.region || "Unknown";
                     const population = country.population?.toLocaleString() || "N/A";
                     return (
-                        <div key={index} className='flex flex-col justify-center items-center w-full mx-auto border-2 border-amber-600'>
-                            <div>
-                                <img src={country.flags.png} alt={country.name.common} className='w-100 h-40 object-cover' />
-                            </div>
-                            <div>
-                                <h3>{country.name.common}</h3>
-                                <p>Capital: {capital}</p>
-                                <p>Region: {regionName}</p>
-                                <p>Population: {population}</p>
+                        <div key={index} className='flex flex-col text-center justify-center items-center w-full mx-auto'>
+                            {/* <div> */}
+                            <img src={country.flags.png} alt={country.name.common} className='lg:w-100 w-full h-40 object-cover rounded-t-sm' />
+                            {/* </div> */}
+                            <div className='bg-gray-400 w-full py-10 px-2 rounded-b-sm'>
+                                <h3 className='text-sm font-bold md:text-sm my-1'>{country.name.common}</h3>
+                                <p className='text-sm my-1'>Capital: {capital}</p>
+                                <p className='text-sm my-1'>Region: {regionName}</p>
+                                <p className='text-sm my-1'>Population: {population}</p>
 
-                                <p> Currency: {currency?.name} ({currency?.symbol})</p>
+                                <p className='text-sm my-1'>Currency: {currency?.name} ({currency?.symbol})</p>
                             </div>
                         </div>
                     );
--- countries/src/components/Navbar.jsx
+++ countries/src/components/Navbar.jsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='bg-amber-300'>
-        <ul className='flex justify-between w-[50%] items-center mx-auto bg-amber-950 py-10 px-10'>
+        <ul className='flex justify-between items-center gap-5 mx-auto bg-gray-200 py-10 px-10'>
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/testimonials'>Testimonials</Link>
-            <Link to='/testimonials'>Testimonials</Link>
             <Link to='/countries'>Countries</Link>
+            <div className='flex flex-col gap-0.5'>
+              <div className='w-4 h-0.5 bg-black'></div>
+              <div className='w-4 h-0.5 bg-black'></div>
+              <div className='w-4 h-0.5 bg-black'></div>
+            </div>
         </ul>
     </nav>
   )
